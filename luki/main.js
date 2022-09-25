a28a5cc69a26406302f84f9c0eac5bd78ca19a48
LEEJiHo <destiny3910@gmail.com>
2022-09-25 14:24:39 +0900

    Feat/Main page Skeleton Code

---
+++ luki/main.js
@@ -0,0 +1,47 @@
+window.onload = function() {
+    let header_benner = document.getElementById("benner").src;
+    console.log(header_benner);
+}
+
+function header_edu(){
+    location.href = "test.html";
+}
+
+function header_re(){
+    location.href = "test.html"
+}
+
+function header_share(){
+    location.href= "test.html"
+}
+
+function E1(){
+    header_benner = "./img/main/benner_1.png";
+    window.
+    alert('test');
+}
+
+function E2(){
+    header_benner = "./img/main/benner_2.png";
+    console.log(header_benner);
+}
+
+function E3(){
+    header_benner = "./img/main/benner_3.png";
+}
+
+function E4(){
+    header_benner = "./img/main/benner_4.png";
+}
+
+function E5(){
+    header_benner = "./img/main/benner_5.png";
+}
+
+function E6(){
+    header_benner = "./img/main/benner_6.png";
+}
+
+function E7(){
+    header_benner = "./img/main/benner_7.png";
+}
