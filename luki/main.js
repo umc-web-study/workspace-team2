a054b914edeb22b93a2c76cefaf6723122c14111
LEEJiHo <destiny3910@gmail.com>
2022-09-25 20:08:52 +0900

    Fix/Header Benner

--- luki/main.js
+++ luki/main.js
@@ -1,3 +1,9 @@
+var T;
+
+window.onload = function(){
+    T = document.querySelector(".benner_img > img");
+}
+
 function header_edu(){
     location.href = "test.html";
 }
@@ -11,29 +17,29 @@ function header_share(){
 }
 
 function E1(){
-    document.getElementById("benner").src = "./img/main/benner_1.png";
+    T.src = "./img/main/benner_1.png";
 }
 
 function E2(){
-    document.getElementById("benner").src = "./img/main/benner_2.png";
+    T.src = "./img/main/benner_2.png";
 }
 
 function E3(){
-    document.getElementById("benner").src = "./img/main/benner_3.png";
+    T.src = "./img/main/benner_3.png";
 }
 
 function E4(){
-    document.getElementById("benner").src = "./img/main/benner_4.png";
+    T.src = "./img/main/benner_4.png";
 }
 
 function E5(){
-    document.getElementById("benner").src = "./img/main/benner_5.png";
+    T.src = "./img/main/benner_5.png";
 }
 
 function E6(){
-    document.getElementById("benner").src = "./img/main/benner_6.png";
+    T.src = "./img/main/benner_6.png";
 }
 
 function E7(){
-    document.getElementById("benner").src = "./img/main/benner_7.png";
+    T.src = "./img/main/benner_7.png";
 }
