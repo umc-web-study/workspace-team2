f96d6a99bd0335be02ed8e6e14f7a9a5cc5ae40e
LEEJiHo <destiny3910@gmail.com>
2022-10-09 17:29:58 +0900

    Top nav and benner control

---
+++ luki/WebReact/rookie_inflearn_clonecoding/src/main_benner.jsx
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import styles from './main_benner.module.css';
+
+class MainBenner extends Component{
+    render() {
+        return(
+            <section>
+                <div className={styles.benner_img}>
+                    <img id={styles.benner} src="img/main/benner_1.png" alt='benner'/>
+                </div>
+                <div className={styles.main_benner}>
+                    <div className={styles.control_main}>
+                        <p id={styles.control_location}>1/7</p>
+                        <div id={styles.control_elements}>
+                            <img id = {styles.control_main_left} src="img/main/control_left.png" alt='left'/>
+                        </div> 
+                        <div id={styles.control_elements}>
+                            <img id = {styles.control_main_center} src="img/main/control_center3.png" alt='center'/>
+                        </div>
+                        <div id={styles.control_elements}>
+                            <img id = {styles.control_main_right} src="img/main/control_right.png" alt='right'/>
+                        </div>
+                    </div>
+                    <div className={styles.control_event} style={{borderLeft: '1px solid #ddd', paddingLeft: '1rem'}}>
+                        <div className={styles.events}>
+                            <p>떠먹는 CS</p>
+                        </div>
+                        <div className={styles.events}>
+                            <p>지식공유신청</p>
+                        </div>
+                        <div className={styles.events}>
+                            <p>왕초보 모여라</p>
+                        </div>
+                        <div className={styles.events}>
+                            <p>신규 강의</p>
+                        </div>
+                        <div className={styles.events}>
+                            <p>Top 50</p>
+                        </div>
+                        <div className={styles.events}>
+                            <p>입문 로드맵</p>
+                        </div>
+                        <div className={styles.events}>
+                            <p>인프런은</p>
+                        </div>
+                        <div className={styles.events}>
+                            <p>인프런 동료 찾기</p>
+                        </div>
+                        <div className={styles.control_eventList}>
+                            <img src="./img/main/event_close.png" alt='event_close'/>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <hr style={{marginTop: '1rem'}}/>
+                </div>
+                <div>
+                    <div className={styles.center_catch}>
+                        <p>인프런에서 가치를 높이세요</p>
+                    </div>
+                    <div className={styles.center_search}>
+                        <input id={styles.searchWindow} type="search" placeholder="배우고 싶은 지식을 입력해보세요"/>
+                        <div id={styles.searchTag}>
+                            <div id={styles.Tag}>
+                                #MVC
+                            </div>
+                            <div id={styles.Tag}>
+                                #SpringBoot
+                            </div>
+                            <div id={styles.Tag}>
+                                #JPA
+                            </div>
+                            <div id={styles.Tag}>
+                                #Python
+                            </div>
+                            <div id={styles.Tag}>
+                                #Java
+                            </div>
+                            <div id={styles.Tag}>
+                                #Spring
+                            </div>
+                            <div id={styles.Tag}>
+                                #JavaScript
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        );
+    }
+}
+
+export default MainBenner
