8262db3603e09bbd7d9e403dbf4a55bf376c9cdd
luke0408 <zjvl324@gmail.com>
2022-10-09 17:03:36 +0900

    Feat: [inflab] react version commit

---
+++ luke/react-version/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Inflab from "./inflearn/inflab/inflab";
+
+export default function App() {
+  return (
+    <main>
+      <Inflab />
+    </main>
+  );
+}
---
+++ luke/react-version/src/inflearn/container.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Box = styled.div`
+display: block;
+height: 100%;
+margin: 0 auto;
+padding: 9.9px 32px;
+max-width: 1200px;
+color: #616568;
+font-weight: 400;
+font-size: 14px;
+`;
+
+export default function Container(){
+  return(
+    <Box />
+  );
+}
