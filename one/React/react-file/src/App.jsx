35e184500be29a7d58b9110d23838ff54ba587c7
HyeWon Cho <5876675@gmail.com>
2022-10-09 17:29:12 +0900

    [One] React First clone coding - Header

---
+++ one/React/react-file/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './Header'
+import Nav from './Nav'
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Nav />
+    </>
+  );
+}
+
+export default App;
