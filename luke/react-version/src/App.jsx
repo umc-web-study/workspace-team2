ff81939e631c9107ea507f7ed3a7c5bc15910b58
luke0408 <zjvl324@gmail.com>
2022-10-23 14:45:43 +0900

    Feat : [Curses] [Rodmap] [Curation] view commit

--- luke/react-version/src/App.jsx
+++ luke/react-version/src/App.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import Header from "./inflearn/header/header";
 import Inflab from "./inflearn/inflab/inflab";
 import BannerMain from "./inflearn/main/banner";
+import Curation from "./inflearn/main/curation";
+import Free_curses from "./inflearn/main/free-curses";
+import New_curses from "./inflearn/main/new-curses";
+import Rodmap from "./inflearn/main/rodmap";
 import SearchBar from "./inflearn/main/searchBar";
 import TagSection from "./inflearn/main/tagSection";
+import Welcome_curses from "./inflearn/main/welcome-curses";
 
 export default function App() {
   return (
@@ -13,6 +18,11 @@ export default function App() {
       <BannerMain />
       <SearchBar />
       <TagSection />
+      <Free_curses />
+      <Welcome_curses />
+      <Rodmap />
+      <Curation />
+      <New_curses />
     </>
   );
 }
