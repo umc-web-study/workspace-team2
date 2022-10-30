363272ee46f65163f4158d8104aa227c5fb36799
LEEJiHo <destiny3910@gmail.com>
2022-10-30 11:43:09 +0900

    Feat/Button actions

--- luki/Menu/index.js
+++ luki/Menu/index.js
@@ -70,18 +70,56 @@ const datas = [
     }
 ]
 
+const body = document.querySelector('body');
 const btnWrapper = document.querySelector('.buttons');
-const dataWrapper = document.querySelector('.menu_data');
+let dataWrapper = document.querySelector('.menu_data');
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     cats.forEach(data => {
         let btn = document.createElement('button');
         btn.type = 'button';
         btn.innerHTML = data;
         btn.className = "nav_btn";
 
-        btn.onclick = function(){
-            console.log(data);s
+        btn.onclick = function () {
+            console.log(data);
+            dataWrapper.remove();
+            dataWrapper = document.createElement('div');
+            dataWrapper.className = 'menu_data';
+
+            body.appendChild(dataWrapper);
+
+            datas.forEach(data => {
+                if (btn.innerHTML === data.category) {
+                    let div = document.createElement('div');
+                    div.className = 'menu_element'
+                    let text_div = document.createElement('div');
+                    text_div.className = 'menu_element_txt'
+                    let text = document.createElement('div');
+                    text.className = "top_txt";
+                    let image = document.createElement('img')
+                    image.src = data.img_url;
+                    let title = document.createElement('p')
+                    title.innerHTML = data.title;
+                    title.id = 'title'
+                    let price = document.createElement('p')
+                    price.innerHTML = data.price;
+                    price.id = 'price'
+                    let line = document.createElement('hr');
+                    let desc = document.createElement('p');
+                    desc.innerHTML = data.desc;
+                    desc.id = 'description'
+
+                    div.appendChild(image);
+                    text.appendChild(title);
+                    text.appendChild(price);
+                    text_div.appendChild(text);
+                    text_div.appendChild(line);
+                    text_div.appendChild(desc);
+                    div.appendChild(text_div);
+                    dataWrapper.appendChild(div);
+                }
+            })
         }
 
         btnWrapper.appendChild(btn);
