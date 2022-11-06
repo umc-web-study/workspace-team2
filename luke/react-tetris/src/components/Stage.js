ff8201e4fddbf852f5efffcca6202187f0231adf
luke0408 <zjvl324@gmail.com>
2022-11-06 20:57:40 +0900

    Docs: react note update

--- luke/react-tetris/src/components/Stage.js
+++ luke/react-tetris/src/components/Stage.js
@@ -5,7 +5,9 @@ import Cell from './Cell';
 
 const Stage = ({ stage }) => (
   <StyledStage width={stage[0].length} height={stage.length}>
+
     {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} />))}
+
   </StyledStage>
 );
 
